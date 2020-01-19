167b7e8c4ac1747f29c94f1be5297a654cd88ea2
Robert Howson <rchowson@yahoo.com>
2020-01-19 22:39:32 +1100

    add task def

--- ecs-service-kong.tf
+++ ecs-service-kong.tf
@@ -85,3 +85,55 @@ resource "aws_ecs_service" "kong" {
     "aws_alb.main"
   ]
 }
+
+resource "aws_ecs_task_definition" "kong" {
+  family                = "${var.app_name}"
+  task_role_arn         = "${module.ecs_task_iam.arn}"
+  network_mode          = "awsvpc"
+  container_definitions = <<EOF
+[
+  {
+    "name": "${var.app_name}",
+    "container_name": "${var.app_name}",
+    "image": "${var.app_image}",
+    "memoryReservation": ${var.container_memory_reservation},
+    "portMappings": [
+      {
+        "ContainerPort": ${var.kong_port_http},
+        "Protocol": "tcp"
+      },
+      {
+        "ContainerPort": ${var.kong_port_https},
+        "Protocol": "tcp"
+      },
+      {
+        "ContainerPort": ${var.kong_port_admin},
+        "Protocol": "tcp"
+      }
+    ],
+    "environment": [
+      {
+        "name"  : "KONG_ADMIN_LISTEN",
+        "value" : "0.0.0.0:${var.kong_port_admin}"
+      },
+      {
+        "name"  : "SSM_PARAMETER_NAME_DB_USERNAME",
+        "value" : "${local.ssm_parameter_name_db_username}"
+      },
+      {
+        "name"  : "SSM_PARAMETER_NAME_DB_PASSWORD",
+        "value" : "${local.ssm_parameter_name_db_password}"
+      },
+      {
+        "name"  : "SSM_PARAMETER_NAME_DB_ENGINE",
+        "value" : "${local.ssm_parameter_name_db_engine}"
+      },
+      {
+        "name"  : "SSM_PARAMETER_NAME_DB_HOST",
+        "value" : "${local.ssm_parameter_name_db_host}"
+      }
+    ]
+  }
+]
+EOF
+}
