708a335000840445a3aab6fb20efcf420ae32ac9
Robert Howson <rchowson@yahoo.com>
2020-01-19 18:03:35 +1100

    delete unused parms

--- vpc.tf
+++ vpc.tf
@@ -16,28 +16,11 @@ variable "private_subnet_cidrs" {
 }
 
 variable "availability_zones" {
-  type = "string"
+#  type = "string"
 }
 module "vpc" {
   source                    = "modules/vpc"
-
-  name                      = "${var.app_name} VPC"
   cidr                      = "${var.vpc_cidr}"
-  map_public_ip_on_launch   = false
+  environment               = "${var.environment}"
 
-  azs                       = "${data.aws_availability_zones.available.names}"
-  private_subnets           = "${var.private_subnet_cidrs}"
-  public_subnets            = "${var.public_subnet_cidrs}"
-
-  enable_nat_gateway        = true
-  single_nat_gateway        = false
-  one_nat_gateway_per_az    = true
-  enable_vpn_gateway        = false
-  enable_dns_support        = true
-  enable_dns_hostnames      = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
 }
